7f6ca19b9830f285afb4104e70facd03015f3c83
bara19kyl <92812604+bara19kyl@users.noreply.github.com>
2025-05-01 14:05:06 +0700

    Add files via upload
    file migration database hari kamis

---
+++ app/Database/Migrations/2025-05-01-060100_CreateUserTable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'            => ['type' => 'INT', 'auto_increment' => true],
+            'user_id'       => ['type' => 'INT'],
+            'tanggal_order' => ['type' => 'DATE'],
+            'total'         => ['type' => 'INT'],
+            'status'        => ['type' => 'ENUM', 'constraint' => ['pending', 'selesai'], 'default' => 'pending'],
+            'created_at'    => ['type' => 'DATETIME', 'null' => true],
+            'updated_at'    => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('orders');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('orders');
+    }
+}
