e635ed47d4d263ae62b02e9a8efbc6180e8c4361
arzl7060 <arzl7060@gmail.com>
2025-05-29 13:00:19 +0700

    same

---
+++ app/Models/LaporanModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class LaporanModel extends Model
+{
+    public function DataHarian($tgl)
+    {
+        return $this->db->table('rincian')
+            ->join('produk', 'produk.kode_produk=rincian.kode_produk')
+            ->join('orders', 'orders.no_faktor=rincian.no_faktor')
+            ->where('orders.tanggal', $tgl)
+            ->select('rincian.kode_produk')
+            ->select('produk.nama_produk')
+            ->select('rincian.harga_jual')
+            ->groupBy('rincian.kode_produk')
+            ->selectSum('rincian.qty')
+            ->selectSum('rincian.total_harga')
+            ->get()->getResultArray();
+    }
+
+    public function DataBulanan($bulan, $tahun)
+    {
+        return $this->db->table('rincian')
+            ->join('orders', 'orders.no_faktur=rincian.no_faktor')
+            ->where('month(orders.tanggal)', $bulan)
+            ->where('year(orders.tanggal)', $tahun)
+            ->select('orders.tanggal')
+            ->groupBy('orders.tanggal')
+            ->selectSum('rincian.total_harga')
+            ->get()->getResultArray();
+    }
+
+    public function DataTahunan($tahun)
+    {
+        return $this->db->table('rincian')
+            ->join('orders', 'orders.no_faktur=rincian.no_faktor')
+            ->where('year(orders.tanggal)', $tahun)
+            ->select('month(orders.tanggal) as bulan')
+            ->groupBy('month(orders.tanggal)')
+            ->selectSum('rincian.total_harga')
+            ->get()->getResultArray();
+    }
+}
