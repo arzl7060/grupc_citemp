1303dd0eb02f6ee1ecf2247d4f94a48f962cd834
arzl7060 <arzl7060@gmail.com>
2025-05-30 18:12:37 +0700

    same

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.69.3/';
+    public string $baseURL = 'https://192.168.69.3/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
